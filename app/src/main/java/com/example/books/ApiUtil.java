112f94072702dbb25d422c7fff71031b7b838195
Charmaine <chamainemush@gmail.com>
2020-07-12 02:14:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/books/ApiUtil.java
@@ -0,0 +1,56 @@
+package com.example.books;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class ApiUtil {
+    private ApiUtil() {
+    }
+
+    public static final String BASE_API_URL = "https://www.google.com/search?tbm=bks&q=api";
+    public static final String QUERY_PARAMETER_KEY = "q";
+    public static final String KEY = "key";
+    public static final String API_KEY = "AIzaSyBg4Th7T375Qzrbknh5pKZ0xda95dWNcn8";
+    public static URL buildUrl(String title) {
+
+        URL url = null;
+        Uri uri = Uri.parse(BASE_API_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAMETER_KEY, title)
+                .appendQueryParameter(KEY, API_KEY)
+                .build();
+        try {
+                url = new URL(uri.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+    static String getJson(URL url) throws IOException {
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream stream = connection.getInputStream();
+            Scanner scanner = new Scanner(stream);
+            scanner.useDelimiter("\\A");
+            boolean hasData = scanner.hasNext();
+            if (hasData) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            Log.d("Error", e.toString());
+            return null;
+
+        } finally {
+            connection.disconnect();
+        }
+    }
+}
