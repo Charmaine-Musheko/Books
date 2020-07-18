d5fad9aad362617b9fd3acd455f4ed8b488e2a96
Charmaine <chamainemush@gmail.com>
2020-07-18 23:30:24 +0200

    .k,

--- app/src/main/java/com/example/books/ApiUtil.java
+++ app/src/main/java/com/example/books/ApiUtil.java
@@ -3,17 +3,25 @@ package com.example.books;
 import android.net.Uri;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+import org.json.JSONTokener;
+import org.json.*;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ApiUtil {
     private ApiUtil() {
     }
 
-    public static final String BASE_API_URL = "https://www.google.com/search?tbm=bks&q=api";
+    public static final String BASE_API_URL = "https://www.googleapis.com/books/v1/volumes/?q=android";
     public static final String QUERY_PARAMETER_KEY = "q";
     public static final String KEY = "key";
     public static final String API_KEY = "AIzaSyBg4Th7T375Qzrbknh5pKZ0xda95dWNcn8";
@@ -52,5 +60,44 @@ public class ApiUtil {
         } finally {
             connection.disconnect();
         }
+    }
+    public static ArrayList<Book> getBooksFromJson(String json){
+        final String ID = "id";
+        final String TITLE = "title";
+        final String SUBTITLE = "subTitle";
+        final String AUTHORS = "authors";
+        final String PUBLISHER = "publisher";
+        final String PUBLISHED_DATE = "publishedDate";
+        final String ITEMS = "items";
+        final String VOLUMEINFO = "volumeInfo";
+        ArrayList<Book> books= new ArrayList<Book>();
+        try{
+            JSONObject jsonBooks = new JSONObject(json);
+            JSONArray arrayBooks = jsonBooks.getJSONArray(ITEMS);
+            int numberOfBooks = arrayBooks.length();
+            for (int i =0; i<numberOfBooks;i++){
+                JSONObject bookJSON = arrayBooks.getJSONObject(i);
+                JSONObject volumeInfoJSON = bookJSON.getJSONObject(VOLUMEINFO);
+                int authorNum = volumeInfoJSON.getJSONArray(AUTHORS).length();
+                String[] authors = new String[authorNum];
+                for (int j = 0; j<authorNum; j++) {
+                    authors[j] = volumeInfoJSON.getJSONArray(AUTHORS).get(j).toString();
+                }
+                Book book = new Book (
+                    bookJSON.getString(ID),
+                    volumeInfoJSON.getString(TITLE),
+                            (volumeInfoJSON.isNull(SUBTITLE)?"":volumeInfoJSON.getString(SUBTITLE)),
+                             authors,
+                    volumeInfoJSON.getString(PUBLISHER),
+                    volumeInfoJSON.getString(PUBLISHED_DATE));
+                books.add(book);
+
+                }
+
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return  books;
     }
 }
--- app/src/main/java/com/example/books/BookListActivity.java
+++ app/src/main/java/com/example/books/BookListActivity.java
@@ -1,37 +1,82 @@
 package com.example.books;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
-public class BookListActivity extends AppCompatActivity {
+public class BookListActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
     private ProgressBar mLoadingProgress;
+    private RecyclerView rvBooks;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+        rvBooks = (RecyclerView) findViewById(R.id.rv_books);
         mLoadingProgress = (ProgressBar) findViewById(R.id.pb_loading);
         try {
             URL bookUrl = ApiUtil.buildUrl("cooking");
             new BooksQueryTask().execute(bookUrl);
 
         }
-        catch (Exception e){
+        catch (Exception e) {
             Log.d("error", e.getMessage());
         }
+
+        //create the layoutManager for the books (linear in this case, scrolling vertically
+        LinearLayoutManager booksLayoutManager =
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        rvBooks.setLayoutManager(booksLayoutManager);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.book_list_menu, menu);
+        final MenuItem searchItem=menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        try {
+            URL bookUrl = ApiUtil.buildUrl(query);
+            new BooksQueryTask().execute(bookUrl);
+        }
+        catch (Exception e) {
+            Log.d("error", e.getMessage());
+        }
+
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
+
     public class BooksQueryTask extends AsyncTask<URL, Void, String> {
 
         @Override
-        protected String doInBackground(@org.jetbrains.annotations.NotNull URL... urls) {
+        protected String doInBackground(URL... urls) {
             URL searchURL = urls[0];
             String result = null;
             try {
@@ -42,24 +87,31 @@ public class BookListActivity extends AppCompatActivity {
             }
             return result;
         }
+
         @Override
         protected void onPostExecute(String result) {
-            TextView tvResult = (TextView) findViewById(R.id.tvResponse);
+
             TextView tvError = (TextView) findViewById(R.id.tv_error);
             mLoadingProgress.setVisibility(View.INVISIBLE);
             if (result == null) {
-                tvResult.setVisibility(View.INVISIBLE);
+                rvBooks.setVisibility(View.INVISIBLE);
                 tvError.setVisibility(View.VISIBLE);
-            } else {
-                tvResult.setVisibility(View.VISIBLE);
+            }
+            else {
+                rvBooks.setVisibility(View.VISIBLE);
                 tvError.setVisibility(View.INVISIBLE);
             }
-            tvResult.setText(result);
+            ArrayList<Book> books = ApiUtil.getBooksFromJson(result);
+            String resultString = "";
 
-        } // URL (Uniform Resource Locator)
+            BooksAdapter adapter = new BooksAdapter(books);
+            rvBooks.setAdapter(adapter);
+        }
+
         @Override
-        protected void onPreExecute(){
+        protected void onPreExecute() {
             super.onPreExecute();
             mLoadingProgress.setVisibility(View.VISIBLE);
         }
-}}
+    }
+}
